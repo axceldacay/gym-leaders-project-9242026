420c255d7866585bc2ef8014a7085cf7c55e8773
dacay_a <dacay_a@f213.ub.lan>
2026-09-24 09:26:33 +0800

    feat: replaced generated angular components with existing src file from previous project and added hoenn leaders component

---
+++ src/app/hoenn/hoenn.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-hoenn',
+  styleUrl: './hoenn.css',
+  templateUrl: './hoenn.html',
+})
+export class Hoenn {}
